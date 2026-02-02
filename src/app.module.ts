1b66896ba27b52ad5dd42f8d4c2c0c072c0245ec
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-02-02 17:31:40 +0330

    TypeOrm.config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
-
+import {ConfigModule} from "@nestjs/config"
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { TypeOrmConfig } from './config/typeoem.config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:join(process.cwd(),".env")
+    }),
+    TypeOrmModule.forRoot(TypeOrmConfig())
+  ],
   controllers: [],
   providers: []
 })
