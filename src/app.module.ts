bca5160a1667853007b3f3b3f1ecfa274beb380e
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-02-02 20:07:05 +0330

    user module generated && base.entity added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import {ConfigModule} from "@nestjs/config"
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { TypeOrmConfig } from './config/typeoem.config';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
       envFilePath:join(process.cwd(),".env")
     }),
-    TypeOrmModule.forRoot(TypeOrmConfig())
+    TypeOrmModule.forRoot(TypeOrmConfig()),
+    UserModule
   ],
   controllers: [],
   providers: []
