ed44e5c62a831ee2789f48b92e1679520ed1dd9f
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-02-03 21:47:52 +0330

    usernameValidator method added to auth service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,14 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { AuthDto } from './dto/auth.dto';
-import { urlEncoded } from 'src/common/constants/constants';
+import { Json, urlEncoded } from 'src/common/constants/constants';
 
 @Controller('auth')
 @ApiTags("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('user-existance')
-  @ApiConsumes(urlEncoded)
+  @ApiConsumes(urlEncoded , Json)
   userExistance(@Body() authDto:AuthDto){
     return this.authService.userExistance(authDto)
   }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { AuthTypes } from 'src/common/enums/type.enum';
+import { AuthMethod } from 'src/common/enums/method.enum';
+import { isEmail } from 'class-validator';
 
 @Injectable()
 export class AuthService {
     userExistance( authDto: AuthDto) {
-        return authDto
-      }
+        const {method , type , username} = authDto
+        switch (type) {
+            case AuthTypes.LOGIN:
+                return this.login(method,username)
+            case AuthTypes.REGISTER:
+                return this.register(method,username)
+            default:
+                throw new UnauthorizedException()
+        }
+    }
+    login(method:AuthMethod , username:string){
+        return this.usernameValidator(method,username)
+    }
+    register(method:AuthMethod , username:string){
+
+    }
+    usernameValidator(method:AuthMethod,username:string){
+        switch (method) {
+            case AuthMethod.EMAIL:
+                if(isEmail(username)) return username
+                throw new BadRequestException("email is not correct")
+            case AuthMethod.USERNAME:
+            return username
+            default:
+                throw new UnauthorizedException("username data is not valid")
+        }
+    }
 }
