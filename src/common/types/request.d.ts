dcbd5a9fd9fcdb4224e3e97eaf86803197e3a9f1
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-02-05 15:29:01 +0330

    user extended in express request

---
+++ src/common/types/request.d.ts
@@ -0,0 +1,9 @@
+import { Request } from 'express';
+import { UserEntity } from 'src/modules/user/entities/user.entity';
+declare global {
+    namespace Express {
+        interface Request {
+            user?:UserEntity
+        }
+    }
+}
