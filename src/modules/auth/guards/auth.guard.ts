2f99886e3d93e531ad4c63a27009358377a70e1a
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-02-10 11:42:54 +0330

    user extending Request issue resolved

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -3,19 +3,20 @@ import { isJWT } from "class-validator";
 import { Request } from "express";
 import { AuthMessage } from "src/common/enums/message.enum";
 import { AuthService } from "../auth.service";
+import { AuthRequest } from "src/common/types/authRequest.type";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private authService :AuthService){}
     async canActivate(context:ExecutionContext){
         const httpContext = context.switchToHttp()
-        const request:Request = httpContext.getRequest<Request>()
+        const request:AuthRequest = httpContext.getRequest<AuthRequest>()
        const token = this.extractToken(request)
         request.user = await this.authService.validateAccessToken(token)
         return true
     }
 
-    protected extractToken(request:Request){
+    protected extractToken(request:AuthRequest){
         const {authorization} = request.headers
         if(!authorization || authorization?.trim()=="") throw new UnauthorizedException(AuthMessage.LOGIN_REQUIRED)
         const [bearer, token] =authorization?.split(" ")
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable, Scope } from '@nestjs/common';
+import { Inject, Injectable, Scope, UnauthorizedException } from '@nestjs/common';
 import { ProfileDto } from './dto/profile.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { ProfileEntity } from './entities/profile.entity';
 import { REQUEST } from '@nestjs/core';
+import type{ AuthRequest } from 'src/common/types/authRequest.type';
 
 
 @Injectable({scope:Scope.REQUEST})
@@ -12,11 +13,13 @@ export class UserService {
   constructor(
     @InjectRepository(UserEntity) private userRepo:Repository<UserEntity>,
     @InjectRepository(ProfileEntity) private ProfileRepo:Repository<ProfileEntity>,
-    @Inject(REQUEST) private request:Request
+    @Inject(REQUEST) private request:AuthRequest
   ){
 
   }
  async changeProfile(Dto:ProfileDto){
-
+  const user = this.request.user;
+  if (!user) throw new UnauthorizedException();
+  const { id } = user;
  }
 }
