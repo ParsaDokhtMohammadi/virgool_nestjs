fb628d9984a02ac6b2c62cd2cac352d8b86eef1f
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-02-23 11:43:31 +0330

    fk added to migration

--- src/migration/1771774963800-$npm_config_name.ts
+++ src/migration/1771774963800-$npm_config_name.ts
@@ -1,43 +1,56 @@
 import { EntityNames } from "src/common/enums/entity.enum";
 import { ROLES } from "src/common/enums/role.enum";
 import { USER_STATUS } from "src/common/enums/userStatus.enum";
-import { MigrationInterface, QueryRunner, Table, TableColumn } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey } from "typeorm";
 
-export class  $npmConfigName1771774963800 implements MigrationInterface {
+export class $npmConfigName1771774963800 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name:EntityNames.USER,
-                columns:[
-                    {name:"id",isPrimary:true,type:"serial",isNullable:false},
-                    {name:"username",type:"varchar(50)",isNullable:true,isUnique:true},
-                    {name:"email",type:"varchar(100)",isNullable:false,isUnique:true},
-                    {name:"new_email",type:"varchar(100)",isNullable:true,isUnique:true},
-                    {name:"role",type:"enum",enum:[ROLES.ADMIN,ROLES.USER]},
-                    {name:"status",type:"enum",enum:[USER_STATUS.BLOCKED,USER_STATUS.REPORT],isNullable:true , default:null},
-                    {name:"password",type:"varchar(20)",isNullable:false},
-                    {name:"created_at",type:"timestamp",default:"now()"}
+                name: EntityNames.USER,
+                columns: [
+                    { name: "id", isPrimary: true, type: "serial", isNullable: false },
+                    { name: "username", type: "varchar(50)", isNullable: true, isUnique: true },
+                    { name: "email", type: "varchar(100)", isNullable: false, isUnique: true },
+                    { name: "new_email", type: "varchar(100)", isNullable: true, isUnique: true },
+                    { name: "role", type: "enum", enum: [ROLES.ADMIN, ROLES.USER] },
+                    { name: "status", type: "enum", enum: [USER_STATUS.BLOCKED, USER_STATUS.REPORT], isNullable: true, default: null },
+                    { name: "password", type: "varchar(20)", isNullable: false },
+                    { name: "created_at", type: "timestamp", default: "now()" }
                 ]
-            }),true
+            }), true
         )
-        const balance = await queryRunner.hasColumn(EntityNames.USER,"balance")
+        const balance = await queryRunner.hasColumn(EntityNames.USER, "balance")
         //@ts-ignore
-        if(!balance) await queryRunner.addColumn(EntityNames.USER,{name:"balance",type:"numeric",default:0})
-        const username = await queryRunner.hasColumn(EntityNames.USER,"username")
-        //if i want to run this i need to comment the queries above
-        if(username) await queryRunner.changeColumn(EntityNames.USER,"username",new TableColumn({
-            name:"username",
-            isNullable:false,
-            isUnique:true,
-            type:"varchar(50)"
+        if (!balance) await queryRunner.addColumn(EntityNames.USER, { name: "balance", type: "numeric", default: 0 })
+
+        await queryRunner.createTable(new Table({
+            name: EntityNames.PROFILE,
+            columns: [
+                { name: "id", isPrimary: true, type: "serial", isNullable: false },
+                { name: "nick_name", type: "varchar(50)", isNullable: true },
+                { name: "user_id", type: "int", isNullable: true , isUnique:true},
+                { name: "bio", type: "varchar", isNullable: true },
+                { name: "image_profile", type: "varchar", isNullable: true },
+
+            ]
+        }))
+        await queryRunner.createForeignKey(EntityNames.PROFILE , new TableForeignKey({
+            columnNames:['user_id'],
+            referencedColumnNames:['id'],
+            referencedTableName:EntityNames.USER,
+            onDelete:"CASCADE"
         }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-       await queryRunner.dropTable(EntityNames.USER ,true)
-       await queryRunner.dropColumn(EntityNames.USER,"balance")
-    
+        const profile = await queryRunner.getTable(EntityNames.PROFILE)
+        const userFK = profile?.foreignKeys.find(fk=>fk.columnNames.indexOf("user_id")!== -1)
+        if(userFK) await queryRunner.dropForeignKey(EntityNames.PROFILE,userFK)
+        await queryRunner.dropTable(EntityNames.USER, true)
+        await queryRunner.dropColumn(EntityNames.USER, "balance")
+
     }
 
 }
